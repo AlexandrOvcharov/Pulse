2e053363e0118a317ca38805cd235004b0fa65ca
Александр <alexandr.ovcharov.front@gmail.com>
2023-04-05 14:26:09 +0600

    made first screen

---
+++ dist/assets/js/main.js
@@ -0,0 +1,20 @@
+const app = console('priver');
+const app2 = console('priver');
+const app3 = console('priver');
+const app4 = console('priver');
+const app5 = console('priver');
+const app6 = console('priver');
+const app = console('priver');
+const app2 = console('priver');
+const app3 = console('priver');
+const app4 = console('priver');
+const app5 = console('priver');
+const app6 = console('priver');
+
+alert("hello world");
+alert("hello world");
+alert("hello world");
+alert("hello world");
+alert("hello world");
+alert("hello world");
+alert("hello world");
---
+++ src/assets/js/main.js
@@ -0,0 +1,10 @@
+//= componets/1.js
+//= componets/2.js
+
+alert("hello world");
+alert("hello world");
+alert("hello world");
+alert("hello world");
+alert("hello world");
+alert("hello world");
+alert("hello world");
