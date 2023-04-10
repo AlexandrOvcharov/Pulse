ed02d89772d98948f48636e708cb5ee0dc93a577
Александр <alexandr.ovcharov.front@gmail.com>
2023-04-10 13:22:26 +0600

    add tiny slider

--- src/assets/js/componets/carousel.js
+++ src/assets/js/componets/carousel.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
-   $('.carousel__inner').slick({
-      prevArrow: '<button type="button" class="slider-arrows slick-prev"><img src="./assets/images/icons/left-arrow.png" alt=""></button>',
-      nextArrow: '<button type="button" class="slider-arrows slider-arrows--right slick-next"><img src="./assets/images/icons/right-arrow.png" alt=""></button>'
-   });
+// $(document).ready(function(){
+//    $('.carousel__inner').slick({
+//       prevArrow: '<button type="button" class="slider-arrows slick-prev"><img src="./assets/images/icons/left-arrow.png" alt=""></button>',
+//       nextArrow: '<button type="button" class="slider-arrows slider-arrows--right slick-next"><img src="./assets/images/icons/right-arrow.png" alt=""></button>'
+//    });
+//  });
+
+const slider = tns({
+   container: '.carousel__inner',
+   items: 1,
+   slideBy: 'page',
+   autoplay: false,
+   controls: false,
+   nav: false
  });
+
+ document.querySelector('.prev').addEventListener('click', function () {
+   slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+   slider.goTo('next');
+});
